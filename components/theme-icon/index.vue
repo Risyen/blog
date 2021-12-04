3b34e594ad844edc806fc2b8d29a1a14bd94f2d6
kanno <812137533@qq.com>
2021-12-04 23:41:48 +0800

    chore:upgrade dependency

--- components/theme-icon/index.vue
+++ components/theme-icon/index.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="theme-icon" title="change theme">
-    <sun size="18" @click="clickHandler" v-show="theme === 'light-theme'" />
-    <moon size="18" @click="clickHandler" v-show="theme === 'dark-theme'" />
+    <Sun size="18" @click="clickHandler" v-show="theme === 'light-theme'" />
+    <Moon size="18" @click="clickHandler" v-show="theme === 'dark-theme'" />
   </div>
 </template>
 
 <script lang="ts">
+import { Sun, Moon } from "@fect-ui/vue-icons";
 export default {
-  name: 'ThemeIcon',
+  name: "ThemeIcon",
+  components: {
+    Sun,
+    Moon,
+  },
   props: {
     theme: String,
   },
-  emits: ['click'],
+  emits: ["click"],
   setup(props, { emit }) {
-    const clickHandler = (e: MouseEvent) => emit('click', e)
+    const clickHandler = (e: MouseEvent) => emit("click", e);
 
     return {
       clickHandler,
-    }
+    };
   },
-}
+};
 </script>
 
 <style scoped>
--- plugins/fect-ui.ts
+++ plugins/fect-ui.ts
@@ -1,6 +1,4 @@
-import Fect from '@fect-ui/vue/lib'
-import Icon from '@fect-ui/vue-icons'
+import Fect from "@fect-ui/vue/lib";
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Fect)
-  nuxtApp.vueApp.use(Icon)
-})
+  nuxtApp.vueApp.use(Fect);
+});
